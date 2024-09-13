86116ae252061b49c70c6fe740d21614263a6690
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-09-13 16:46:56 +0200

    Laravel Bootcamp finished

---
+++ buzz-microblogging/app/Http/Controllers/BuzzController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Buzz;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
+use Illuminate\Support\Facades\Gate;
+use Illuminate\View\View;
+
+class BuzzController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('buzzs.index', [
+            'buzzs' => Buzz::with('user')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $request->user()->buzzs()->create($validated);
+ 
+        return redirect(route('buzzs.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Buzz $buzz)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Buzz $buzz): View
+    {
+        Gate::authorize('update', $buzz);
+ 
+        return view('buzzs.edit', [
+            'buzz' => $buzz,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Buzz $buzz): RedirectResponse
+    {
+        Gate::authorize('update', $buzz);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $buzz->update($validated);
+ 
+        return redirect(route('buzzs.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Buzz $buzz): RedirectResponse
+    {
+        Gate::authorize('delete', $buzz);
+ 
+        $buzz->delete();
+ 
+        return redirect(route('buzzs.index'));
+    }
+}
